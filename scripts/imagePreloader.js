cb0363fe96393c969212981db7d7a552dd5d8ec2
Ulyuly <k_tuszynski@vp.pl>
2022-09-12 00:12:44 +0200

    cc

--- scripts/imagePreloader.js
+++ scripts/imagePreloader.js
@@ -1,6 +1,8 @@
 let pic = new Image();
 
-for (i=0;i<4;i++){
+for (i=1;i<5;i++){
     pic.src = "bin/sliderphoto"+i+".jpeg";
-    console.log("image loaded")
 }
+
+let pic2 = new Image();
+pic2.src = "bin/sliderphoto2.jpeg"
