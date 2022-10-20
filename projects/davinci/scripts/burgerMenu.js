8092aa733ba0893881e225433231cb4af2933953
Ulyuly <k_tuszynski@vp.pl>
2022-10-20 23:31:17 +0200

    Derp, actual portfolio this time
    Accidentally forgot to build the project before pushing it to origin, yikes!

--- projects/davinci/scripts/burgerMenu.js
+++
@@ -1,8 +0,0 @@
-function burgerMenu() {
-    let navBar = document.getElementsByClassName("menuBurgerContents")[0];
-    if(navBar.style.display === "block"){
-        navBar.style.display = "none";
-    } else {
-        navBar.style.display = "block";
-    }
-}
